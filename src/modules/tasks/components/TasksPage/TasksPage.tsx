0116cec779e9577ffb1c5e4c3b7d07ad2f6db3d6
Ivan Harasta <ivan.harasta@ackee.cz>
2023-01-23 18:23:14 +0100

    ✨ Add tasks module

---
+++ src/modules/tasks/components/TasksPage/TasksPage.tsx
@@ -0,0 +1,11 @@
+import { Heading } from 'modules/ui';
+
+export interface TasksPageProps {}
+
+export const TasksPage = ({}: TasksPageProps) => {
+    return (
+        <div>
+            <Heading level={1}>tasks</Heading>
+        </div>
+    );
+};
