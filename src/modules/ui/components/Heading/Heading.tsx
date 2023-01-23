ac9ae971afd320d359c0234bf23b03ef99c99331
Ivan Harasta <ivan.harasta@ackee.cz>
2023-01-23 19:38:34 +0100

    💄 Cannot use template literals when defining tailwind classes

--- src/modules/ui/components/Heading/Heading.tsx
+++ src/modules/ui/components/Heading/Heading.tsx
@@ -6,13 +6,15 @@ export interface HeadingProps {
 }
 
 export const Heading = ({ level, children }: HeadingProps) => {
+    const base = 'font-bold tracking-wide font-mono mb-6';
+
     if (level === 2) {
-        return <h2 className="text-xl font-bold tracking-wide font-mono text-red-400">{children}</h2>;
+        return <h2 className={`text-xl text-red-400 ${base}`}>{children}</h2>;
     }
 
     if (level === 3) {
-        return <h3 className="text-lg font-bold tracking-wide font-mono text-inherit">{children}</h3>;
+        return <h3 className={`text-lg text-inherit ${base}`}>{children}</h3>;
     }
 
-    return <h1 className="text-2xl font-bold tracking-wide font-mono text-red-400">{children}</h1>;
+    return <h1 className={`text-2xl text-red-400 ${base}`}>{children}</h1>;
 };
--- src/modules/ui/components/Paragraph/Paragraph.tsx
+++ src/modules/ui/components/Paragraph/Paragraph.tsx
@@ -3,11 +3,28 @@ import { ReactNode } from 'react';
 export interface ParagraphProps {
     bold?: boolean;
     mt?: 1 | 2 | 3 | 4;
+    light?: boolean;
     children: ReactNode;
 }
 
-export const Paragraph = ({ bold = false, mt = 1, children }: ParagraphProps) => {
-    const classNames = ['tracking-wide', 'font-mono', 'text-slate-600', `mt-${mt}`];
+export const Paragraph = ({ bold = false, mt = 1, light = false, children }: ParagraphProps) => {
+    const classNames = ['tracking-wide', 'font-mono'];
+
+    if (mt === 1) {
+        classNames.push('mt-1');
+    } else if (mt === 2) {
+        classNames.push('mt-2');
+    } else if (mt === 3) {
+        classNames.push('mt-3');
+    } else if (mt === 4) {
+        classNames.push('mt-4');
+    }
+
+    if (light) {
+        classNames.push('text-slate-400');
+    } else {
+        classNames.push('text-slate-600');
+    }
 
     if (bold) {
         classNames.push('font-bold');
