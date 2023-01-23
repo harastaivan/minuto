6e15912e9c3c97aa22bd133ddc529b189fafe7aa
Ivan Harasta <ivan.harasta@ackee.cz>
2023-01-23 18:23:26 +0100

    ✨ Add time tracking module

---
+++ src/modules/time-tracking/components/TimeTrackingPage/TimeTrackingPage.tsx
@@ -0,0 +1,11 @@
+import { Heading } from 'modules/ui';
+
+export interface TimeTrackingPageProps {}
+
+export const TimeTrackingPage = ({}: TimeTrackingPageProps) => {
+    return (
+        <div>
+            <Heading level={1}>time tracking</Heading>
+        </div>
+    );
+};
