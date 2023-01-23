b64829ce8334ccf560bdecf42b99e0e0f360be42
Ivan Harasta <ivan.harasta@ackee.cz>
2023-01-23 18:23:37 +0100

    ✨ Add journal module

---
+++ src/modules/journal/components/JournalPage/JournalPage.tsx
@@ -0,0 +1,11 @@
+import { Heading } from 'modules/ui';
+
+export interface JournalPageProps {}
+
+export const JournalPage = ({}: JournalPageProps) => {
+    return (
+        <div>
+            <Heading level={1}>journal</Heading>
+        </div>
+    );
+};
