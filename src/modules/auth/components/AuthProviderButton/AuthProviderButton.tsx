72a28ac88ae8cfdac4f7e5f96a90c5e568bceb43
Ivan Harasta <ivan@harasta.dev>
2023-02-05 00:29:23 +0100

    💄 Add google icon to auth button

--- src/modules/auth/components/AuthProviderButton/AuthProviderButton.tsx
+++ src/modules/auth/components/AuthProviderButton/AuthProviderButton.tsx
@@ -1,4 +1,5 @@
 import { api } from 'modules/api';
+import { GoogleIcon } from 'modules/ui/modules/icons';
 import type { AuthProvider as Provider } from '../../constants';
 
 export interface AuthProviderButtonProps {
@@ -16,8 +17,9 @@ export const AuthProviderButton = ({ provider }: AuthProviderButtonProps) => {
         <>
             <button
                 onClick={onClick}
-                className="font-mono font-bold bg-slate-200 text-slate-500 px-8 py-2 rounded m-2 hover:text-slate-600 hover:bg-slate-300 transition-colors"
+                className="font-mono font-bold bg-red-200 text-red-500 px-7 py-2 rounded m-2 hover:text-slate-600 hover:bg-slate-300 transition-colors flex items-center gap-3"
             >
+                <GoogleIcon className="w-4 h-4" />
                 login with {provider}
             </button>
         </>
