68bb035717d4b976991dacdf87ee53eecfe60560
Ivan Harasta <ivan@harasta.dev>
2023-02-05 00:29:49 +0100

    💄 Add branding to login page

--- src/modules/auth/components/LoginPage/LoginPage.tsx
+++ src/modules/auth/components/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import { Navigate } from 'react-router-dom';
 
 import { AnonymousLayout } from 'modules/layout';
 import { config } from 'config';
+import { Heading } from 'modules/ui';
 
 import { AuthProviderButton } from '../AuthProviderButton';
 import { useIsAuthenticated } from '../../hooks';
@@ -17,6 +18,15 @@ export const LoginPage = ({}: LoginPageProps) => {
 
     return (
         <AnonymousLayout>
+            <div className="pb-6">
+                <Heading level={1}>
+                    welcome to <span className="text-black">minuto</span>
+                </Heading>
+            </div>
+            <div className="pb-48 text-neutral-500 text-center">
+                <Heading level={3}>plan your day / time tracker / journal desktop app</Heading>
+                <Heading level={3}>made for freelancers</Heading>
+            </div>
             {config.api.providers.map((provider) => {
                 return <AuthProviderButton key={provider} provider={provider} />;
             })}
