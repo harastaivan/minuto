e0214262fc8cbdd7e38229f26c03d6e9722e035f
Ivan Harasta <ivan@harasta.dev>
2023-02-05 00:30:21 +0100

    🐛 Fix forwardRef

--- src/modules/ui/components/Dropdown/DropdownContent/DropdownContent.tsx
+++ src/modules/ui/components/Dropdown/DropdownContent/DropdownContent.tsx
@@ -1,7 +1,8 @@
+import { forwardRef } from 'react';
 import { DropdownMenuContent, type DropdownMenuContentProps } from '@radix-ui/react-dropdown-menu';
 
 export interface DropdownContentProps extends Omit<DropdownMenuContentProps, 'className'> {}
 
-export const DropdownContent = ({ ...props }: DropdownContentProps) => {
-    return <DropdownMenuContent className="bg-slate-300 text-slate-500 px-2 py-2 rounded" {...props} />;
-};
+export const DropdownContent = forwardRef<HTMLDivElement>(({ ...props }: DropdownContentProps, ref) => {
+    return <DropdownMenuContent className="bg-slate-300 text-slate-500 px-2 py-2 rounded" {...props} ref={ref} />;
+});
