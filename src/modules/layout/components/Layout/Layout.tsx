344c6784127d5fc4639a57e2ebeacd894ad45fe8
Ivan Harasta <ivan@harasta.dev>
2023-02-05 00:30:00 +0100

    🐛 Fix overflow scroll

--- src/modules/layout/components/Layout/Layout.tsx
+++ src/modules/layout/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutProps) => {
     return (
         <div className="flex flex-row bg-slate-100 h-screen">
             <Sidebar />
-            <main className="px-6 py-4 w-full">{children}</main>
+            <main className="px-6 py-4 w-full overflow-y-scroll">{children}</main>
         </div>
     );
 };
